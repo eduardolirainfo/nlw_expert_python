75d223eb36acb8aa7cd05a8563142e96b2467573
eduardolirainfo <eduardolirainfo@gmail.com>
2024-02-12 17:57:20 -0300

    use controllers and reallocate drivers

--- src/views/tag_creator_view.py
+++ src/views/tag_creator_view.py
@@ -1,10 +1,13 @@
 from src.views.http_types.http_request import HttpRequest
 from src.views.http_types.http_response import HttpResponse
+from src.controllers.tag_creator_controller import TagCreatorController
 
 
 class TagCreatorView:
 
     def validate_and_create(self, http_request: HttpRequest) -> HttpResponse:
+        tag_creator_controller = TagCreatorController()
+
         body = http_request.body
         product_code = body['product_code']
 
@@ -13,9 +16,10 @@ class TagCreatorView:
                 status_code=400,
                 body={'message': 'Product code is required!'}
                 )
-        print(f'Creating tag for product code: {product_code}')
+
+        formatted_response = tag_creator_controller.create(product_code)
 
         return HttpResponse(
             status_code=200,
-            body={'message': 'Tag created successfully!'}
+            body=formatted_response
             )

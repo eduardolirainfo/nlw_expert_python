8380f4032dc393172dde1833a18c2403382324a5
eduardolirainfo <eduardolirainfo@gmail.com>
2024-02-12 15:35:58 -0300

    use src structure

--- src/main/routes/tag_routes.py
+++ src/main/routes/tag_routes.py
@@ -1,9 +1,9 @@
 from flask import Blueprint, request, jsonify
 
-tag_routes_bp = Blueprint('tag_routes', __name__)
+tags_routes_bp = Blueprint('tags_routes', __name__)
 
 
-@tag_routes_bp.route('/create_tag', methods=['POST'])
+@tags_routes_bp.route('/create_tag', methods=["POST"])
 def create_tag():
     print(request.json)
     return jsonify({'message': 'Tag created successfully!'}), 201
